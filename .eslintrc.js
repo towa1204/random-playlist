4ba5b6cb5e3a27967d07c7030e4b086aee950c20
lsadsfj <lsadsfj@gmail.com>
2023-03-24 22:26:02 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 'off',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
---
+++ README.md
@@ -0,0 +1,24 @@
+# clasp-typescript-template
+
+GAS 開発用のテンプレートリポジトリ
+
+- TypeScript
+- ESLint
+- Prettier
+- Jest
+- @types/google-apps-script
+
+# usage
+
+git clone and npm install
+
+## clasp
+
+clasp はグローバルインストールされていることを想定
+
+`npm install -g @google/clasp`
+`clasp login`
+
+## test
+
+jest の使い方は https://typescriptbook.jp/tutorials/jest を参照
